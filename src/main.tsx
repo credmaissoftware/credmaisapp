b5cfd1d493cfdc3868ba65d6983a1f1c0338d620
credmaissoftware <327851475+credmaissoftware@users.noreply.github.com>
2026-09-11 13:27:38 -0300

    feat: unificar acabamento visual das paginas

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./credinho.css";
 import "./glass-overrides.css";
+import "./workspace-overrides.css";
 import "./menu-icons.css";
 
 // Identifica esta publicação e garante um novo arquivo de entrada quando o CDN
